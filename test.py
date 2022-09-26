436776c1ba553e54956ffad666f5ee4aaec78b04
Mangesh Sharma <mangeshsharma4567@gmail.com>
2022-09-26 15:14:43 +0530

    Added doc2html function

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
-from general_toolkit import doc_tools,doc2pdf
+from general_toolkit import doc2html,doc2pdf,doc_tools
 import os
-x = doc_tools.scan_doc_from_image("D:\\HandGestures\\images\\cards.jpg", [[327,207],[556,273],[461,591],[237,526]],verbose=False, w=100, h=100)
-print(x)
-
-directory = 'D:\\General toolkit'
-for filename in os.listdir(directory):
-    if filename.endswith("doc"):
-        print(filename)
-        doc2pdf.convertingpdf(directory+"\\"+filename, ".doc")
-    elif filename.endswith("docx"):
-        print(filename)
-        doc2pdf.convertingpdf(directory+"\\"+filename, ".docx")
+def test1():
+    x = doc_tools.scan_doc_from_image("D:\\HandGestures\\images\\cards.jpg", [[327,207],[556,273],[461,591],[237,526]],verbose=False, w=100, h=100)
+    print(x)
+def test2():
+    directory = 'D:\\General toolkit'
+    for filename in os.listdir(directory):
+        if filename.endswith("doc"):
+            print(filename)
+            doc2pdf.convertingpdf(directory+"\\"+filename, ".doc")
+        elif filename.endswith("docx"):
+            print(filename)
+            doc2pdf.convertingpdf(directory+"\\"+filename, ".docx")
+def test3():
+    directory = 'D:\\Python'
+    for filename in os.listdir(directory):
+        if filename.endswith("doc"):
+            print(filename)
+            doc2pdf.convert2html(directory+"\\"+filename, ".doc")
+        elif filename.endswith("docx"):
+            print(filename)
+            doc2html.convert2html(directory+"\\"+filename, ".docx")
+test3()
